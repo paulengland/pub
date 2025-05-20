91b23e5b0187428fe68384d18f1eb35ae3033cca
Paul England <england.paul@gmail.com>
2025-05-20 16:33:11 -0700

    Update init-enc.sh

--- init-enc.sh
+++ init-enc.sh
@@ -7,7 +7,9 @@
 # --- Configuration ---
 DEVICE="/dev/vdc"  # Replace with the actual device you want to encrypt
 KEYFILE="/root/luks.key"   # Path to the keyfile
-MNT_POINT="/datica"      # Mount point for the encrypted volume
+MNT_ROOT="/datica"
+MNT_POINT="$MNT_ROOT/enc1"      # Mount point for the encrypted volume
+MNT_POINT2="$MNT_ROOT/int2"      # Mount point for the encrypted volume
 VOLUME_NAME="datica"     # Name of the LUKS volume
 
 # --- Create Keyfile ---
@@ -28,13 +30,19 @@ echo "Formatting the encrypted volume: /dev/mapper/$VOLUME_NAME"
 mkfs.ext4 "/dev/mapper/$VOLUME_NAME"  # Format as ext4 (change as needed)
 
 # --- Create Mount Point ---
-echo "Creating mount point: $MNT_POINT"
+echo "Creating encrypted mount point: $MNT_POINT"
 mkdir -p "$MNT_POINT"  # Create the mount point directory
 
 # --- Mount the Encrypted Volume ---
 echo "Mounting encrypted volume to: $MNT_POINT"
 mount "/dev/mapper/$VOLUME_NAME" "$MNT_POINT"  # Mount the decrypted volume
+echo "LUKS encrypted volume successfully mounted on $MNT_POINT"
 
 chmod 777 $MNT_POINT
 
-echo "LUKS encrypted volume successfully mounted on $MNT_POINT"
+# --- Mount the integrity protected volume (no dm-verity yet)
+echo "Mounting measured volume to: $MNT_POINT2"
+mkdir -p "$MNT_POINT2"  # Create the mount point directory
+mount /dev/mapper/vdc "$MNT_POINT2"  # Mount the decrypted volume
+echo "measured  encrypted volume successfully mounted on $MNT_POINT2scd"
+
